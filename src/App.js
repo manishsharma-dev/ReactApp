d485f4aa94575bf2dc789727e13c909e2393b0f4
Manish Sharma <manish.sharma@xpenseco.com>
2020-08-27 22:18:43 +0530

    components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import Ninjas from './ninjas';
 
-function App() {
-  return (
-    <div className="App">
-    <h1>My First React App</h1>
-    <p>Welcome</p>
-    </div>
-  );
+
+
+class App extends Component {
+  state = {
+    ninjas : [
+        {name : 'Light', age : 21, skill : 'Tennis', id : 1},
+        {name : 'Ryujaki', age : 26, skill : 'Soccer', id : 2},
+        {name : 'ruyga', age : 34, skill : 'Golf', id : 3}
+    ]
+  }
+  render() {
+    return (
+      <div className="App">
+      <h1>My First React App</h1>
+      <p>Welcome</p>
+      <Ninjas ninjas={this.state.ninjas} />
+      </div>
+    )
+  }
 }
 
 export default App;
