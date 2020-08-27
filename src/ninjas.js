8b73a1434d1c228f61e060f1e59febe5c093fd04
Manish Sharma <manish.sharma@xpenseco.com>
2020-08-27 22:36:32 +0530

    conditional output

--- src/ninjas.js
+++ src/ninjas.js
@@ -1,25 +1,53 @@
 import React from 'react';
 
 
-const Ninjas = (props) => {
+const Ninjas = ({ ninjas }) => {
 
-        // console.log(this.props);
-        const { ninjas } = props;
-        const ninjaList = ninjas.map(ninja => {
-            return (
-                <div className='ninja' key={ninja.id}>
-                    <div>Name : {ninja.name}</div>
-                    <div>Age : {ninja.age}</div>
-                    <div>Skill : {ninja.skill}</div>
-                </div>
-            )
-        })
-        return (
-            <div className='ninja-list'>
-                {ninjaList}
-            </div>
-        )
-   
+    // console.log(this.props);
+    // const ninjaList = ninjas.map(ninja => {
+    //     if(ninja.age <30){
+    //         return (
+    //             <div className='ninja' key={ninja.id}>
+    //                 <div>Name : {ninja.name}</div>
+    //                 <div>Age : {ninja.age}</div>
+    //                 <div>Skill : {ninja.skill}</div>
+    //             </div>
+    //         )
+    //     }
+    //     else{
+    //         return null;
+    //     }          
+    // })
+
+    // const ninjaList = ninjas.map(ninja => {
+    //     return ninja.age < 30
+    //         ?(
+    //         <div className='ninja' key={ninja.id}>
+    //             <div>Name : {ninja.name}</div>
+    //             <div>Age : {ninja.age}</div>
+    //             <div>Skill : {ninja.skill}</div>
+    //         </div>
+    //         )
+    //         : null;
+    // })
+    return (
+        <div className='ninja-list'>
+            {
+               ninjas.map(ninja => {
+                return ninja.age < 30
+                    ?(
+                    <div className='ninja' key={ninja.id}>
+                        <div>Name : {ninja.name}</div>
+                        <div>Age : {ninja.age}</div>
+                        <div>Skill : {ninja.skill}</div>
+                    </div>
+                    )
+            : null;
+    })
+            }
+        </div>
+    )
+
 }
 
 export default Ninjas;
