df9b342cb2d094cf214a5eed5e1ed19859d4d8e5
Manish Sharma <manish.sharma@xpenseco.com>
2020-09-13 21:24:57 +0530

    delete operation

--- src/App.js
+++ src/App.js
@@ -13,13 +13,31 @@ class App extends Component {
         {name : 'ruyga', age : 34, skill : 'Golf', id : 3}
     ]
   }
+
+  addNinja = (ninja) => {
+    ninja.id = Math.random();
+    let ninjas = [...this.state.ninjas,ninja];
+    this.setState({
+      ninjas: ninjas
+    })
+  }
+
+  deleteNinja = (id) => {
+     let ninjas = this.state.ninjas.filter((ninja) =>{
+       return ninja.id !== id
+     });
+     this.setState({
+       ninjas : ninjas
+     })
+  }
+
   render() {
     return (
       <div className="App">
       <h1>My First React App</h1>
       <p>Welcome</p>
-      <Ninjas ninjas={this.state.ninjas} />
-      <AddNinja />
+      <Ninjas deleteNinja={this.deleteNinja} ninjas={this.state.ninjas} />
+      <AddNinja addNinja={this.addNinja} />
       </div>
     )
   }
--- src/ninjas.js
+++ src/ninjas.js
@@ -1,45 +1,19 @@
 import React from 'react';
 
 
-const Ninjas = ({ ninjas }) => {
+const Ninjas = ({ ninjas, deleteNinja }) => {
 
-    // console.log(this.props);
-    // const ninjaList = ninjas.map(ninja => {
-    //     if(ninja.age <30){
-    //         return (
-    //             <div className='ninja' key={ninja.id}>
-    //                 <div>Name : {ninja.name}</div>
-    //                 <div>Age : {ninja.age}</div>
-    //                 <div>Skill : {ninja.skill}</div>
-    //             </div>
-    //         )
-    //     }
-    //     else{
-    //         return null;
-    //     }          
-    // })
-
-    // const ninjaList = ninjas.map(ninja => {
-    //     return ninja.age < 30
-    //         ?(
-    //         <div className='ninja' key={ninja.id}>
-    //             <div>Name : {ninja.name}</div>
-    //             <div>Age : {ninja.age}</div>
-    //             <div>Skill : {ninja.skill}</div>
-    //         </div>
-    //         )
-    //         : null;
-    // })
     return (
         <div className='ninja-list'>
             {
                ninjas.map(ninja => {
-                return ninja.age < 30
+                return ninja.age >20
                     ?(
                     <div className='ninja' key={ninja.id}>
                         <div>Name : {ninja.name}</div>
                         <div>Age : {ninja.age}</div>
                         <div>Skill : {ninja.skill}</div>
+                        <button onClick={() => {deleteNinja(ninja.id)}}>Delete ninja</button>
                     </div>
                     )
             : null;
